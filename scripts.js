0c14f1eed6133605b237742479c36664e7cc5a95
Ruben Medina Martinez <ruskymartinez@gmail.com>
2022-08-12 20:58:28 +0200

    Finalización ejercicio 8 JavaScript.

---
+++ scripts.js
@@ -0,0 +1,70 @@
+let date = 1;
+switch (date) {
+    case 1:
+        console.log("Enero");
+        break;
+    case 2:
+        console.log("Febrero");
+        break;
+    case 3:
+        console.log("Marzo");
+        break;
+    case 4:
+        console.log("Abril");
+        break;
+    case 5:
+        console.log("Mayo");
+        break;
+    case 6:
+        console.log("Junio");
+        break;
+    case 7:
+        console.log("Julio");
+        break;
+    case 8:
+        console.log("Agosto");
+        break;
+    case 9:
+        console.log("Septiembre");
+        break;
+    case 10:
+        console.log("Octubre");
+        break;
+    case 11:
+        console.log("Noviembre");
+        break;
+    case 12:
+        console.log("Diciembre");
+        break;
+    default:
+        console.log("No existe este mes");
+        break;
+}
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+let numeroPalabra = "1";
+switch (numeroPalabra) {
+    case "1":
+        numeroPalabra = 1;
+        console.log(numeroPalabra);
+        break;
+    case "2":
+        numeroPalabra = 2;
+        console.log(numeroPalabra);
+        break;
+    case "3":
+        numeroPalabra = 3;
+        console.log(numeroPalabra);
+        break;
+    case "4":
+        numeroPalabra = 4;
+        console.log(numeroPalabra);
+        break;
+    case "5":
+        numeroPalabra = 5;
+        console.log(numeroPalabra);
+        break;
+    default:
+        console.log("Introduce un numero entre el 1 y el 5.");
+        break;
+}
